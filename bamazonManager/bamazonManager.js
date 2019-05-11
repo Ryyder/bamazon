33938ee8d1690605551711dd1fa0d7ae7e8d56c6
Ryyder <brucecho82@hotmail.com>
2019-05-11 11:23:24 -0700

    have addInv function working correctly

--- bamazonManager/bamazonManager.js
+++ bamazonManager/bamazonManager.js
@@ -22,6 +22,7 @@ connection.connect(function(err) {
   managerStart();
 });
 
+//our starting prompts for the user
 const managerStart = () => {
   inquirer
     .prompt({
@@ -31,7 +32,7 @@ const managerStart = () => {
       choices: ["View Products", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"]
     })
     .then(function(answer) {
-      
+      //logic to determine which function to call, based on user input
       if (answer.choice == "View Products") {
         console.log("Viewing products");
         selectAll();
@@ -45,6 +46,7 @@ const managerStart = () => {
       else if (answer.choice == "Add New Product") {
         addProd();
       }
+      //end our connection when the user selects exit
       else{
         connection.end();
       }
@@ -56,6 +58,7 @@ const selectAll = () => {
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     console.table(res);
+    //start our prompt
     managerStart();
     });
 }
@@ -65,6 +68,7 @@ const lowInv = () => {
   connection.query("SELECT * FROM products WHERE stock_quantity < 5", function(err, res) {
     if (err) throw err;
     console.table(res);
+    //take us back to our prompt
     managerStart();
   });
 
@@ -72,6 +76,7 @@ const lowInv = () => {
 
 //updates our database depending on the item they select in the db
 const addInv = () => {
+  //prompt user for the item name
   inquirer
     .prompt([
       {
@@ -80,6 +85,7 @@ const addInv = () => {
         message: "Enter the item you would like to add more quantity to"
       },
       {
+      //prompt user for quantity they want to add
       name: "add",
       type: "input",
       message: "How many items would you like to add?",
@@ -92,28 +98,39 @@ const addInv = () => {
       }
   ])
   .then(function(answer) {
-    console.log(answer.name);
-    console.log(answer.add);
-    var itemAdd = parseInt(answer.add);
-    var query = connection.query(
-      "UPDATE products SET ? WHERE ?",
-      [
-        {
-          stock_quantity: itemAdd
-        },
-        {
-          product_name: answer.name
-        }
-      ],
-      function(err) {
-        if (err) throw err;
-        console.log(query.sql);
-        managerStart();
+    //query our database to get stock quantity from our products table where product name = whatever the user entered
+    connection.query("SELECT stock_quantity FROM products WHERE ?", [
+      {
+        product_name: answer.name
       }
-    );
-  });
+    ],function(err, res) {
+      if(err) throw err;
+      //increase the stock quantity amount according to what the user entered
+      var itemAdd = parseInt(res[0].stock_quantity) + parseInt(answer.add);
+      //our update query, update our stock_quantity according to what the user entered, where the product_name set to what the user entered
+      var query = connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+          {
+            stock_quantity: itemAdd
+          },
+          {
+            product_name: answer.name
+          }
+        ],
+        function(err) {
+          if (err) throw err;
+          console.log(query.sql);
+          //take us back to our prompt
+          managerStart();
+        }
+      )
 
+    });
+
+  });
 }
+
 
 const addProd = () => {
 
