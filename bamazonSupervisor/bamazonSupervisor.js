67fae979bd84f9f9bcc23d178fe1a8163089c3d2
Ryyder <brucecho82@hotmail.com>
2019-05-14 18:36:34 -0700

    working connection to db

---
+++ bamazonSupervisor/bamazonSupervisor.js
@@ -0,0 +1,18 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var cTable = require("console.table");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Jaeger18121!",
+  database: "bamazon"
+});
